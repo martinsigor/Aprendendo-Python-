7fbe164c1d350e229399a0a8d210f5fa045c25e1
Igor  M. S. Oliveira <igormartinsdasilvaoliveira@gmail.com>
2021-12-01 21:12:53 -0300

    Delete Folder directory

--- Folder/4.Laços_e_repetições(for).py
+++
@@ -1,6 +0,0 @@
-tabuada=int(input("Digite um número para exibir a tabuada: "))
-
-print("Tabuada do número ", tabuada)
-
-for valor in range(1,11,1): #No for (inicio , fim , incremento )
-    print(str(tabuada)+" x "+str(valor) + " = " + str((tabuada*valor)))
