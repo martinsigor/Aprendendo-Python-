3075e3916d668690101fb17f17d859dc064497e2
Igor  M. S. Oliveira <igormartinsdasilvaoliveira@gmail.com>
2021-12-02 19:48:23 -0300

    Add files via upload

---
+++ Capitulo3/5.Modulo.principal.py
@@ -0,0 +1,18 @@
+from Capitulo3.Funcoes_e_modularização import *
+
+minhaLista=[]
+print("Preenchendo")
+preencherInventario(minhaLista)
+print("Exibindo")
+exibirInventario(minhaLista)
+
+print("Pesquisando")
+localizarPorNome(minhaLista)
+print("Alterando")
+depreciarPorNome(minhaLista, 20)
+
+print("Excluindo")
+print(excluirPorSerial(minhaLista))
+exibirInventario(minhaLista)
+print("Resumindo")
+resumirValores(minhaLista)
