54d196907675fdf4488903e86a56011aa1a269f6
Igor  M. S. Oliveira <igormartinsdasilvaoliveira@gmail.com>
2021-12-01 21:06:10 -0300

    Add files via upload

---
+++ 1.Conceitos_e_tipos_de_variaveis.py
@@ -0,0 +1,8 @@
+nome=input("Digite um funcionário: ") #string
+empresa=input("Lugar daonde é: ") #string
+qtde_pessoas=int (input("Quantas pessoas tem ai: ")) #int é um numero inteiro
+media_mensalidade=float (input("Média do salário: ")) #float numero com virgula 
+print(nome, "estuda na escolaoi" ,empresa)
+print("Tem", qtde_pessoas,"estudantes")
+print("a media da mensalidade é: ",media_mensalidade)
+print(type(empresa))
